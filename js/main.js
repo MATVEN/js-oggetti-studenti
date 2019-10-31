6d76d092d6c1b4c677caeb65f08a159f28455d90
MATVEN <mattiavena2993@gmail.com>
2019-10-31 09:12:29 +0100

    add track ex

---
+++ js/main.js
@@ -0,0 +1,23 @@
+//Creare un oggetto che descriva uno studente con le seguenti proprietà: nome, cognome e età.
+var student = {
+  "nome":[],
+  "cognome":[],
+  "età":[]
+};
+//Stampare a schermo attraverso il for in tutte le proprietà.
+for (var key in studentt) {
+  console.log(key);
+}
+
+//Creare un array di oggetti di studenti.
+var name = [];
+var age = [];
+var surname = [];
+//Ciclare su tutti gli studenti e stampare per ognuno nome e cognome
+while () {
+
+}
+//Dare la possibilità all’utente attraverso 3 prompt di aggiungere un nuovo oggetto studente inserendo nell’ordine: nome, cognome e età.
+var nameUser = prompt('Inserisci il tuo nome.')
+var surnameUser = prompt('Insrisci il tuo cognome')
+var ageUse = parseInt('Inserisci la tua età.')
