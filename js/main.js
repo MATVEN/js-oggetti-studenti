a329d3f843f51d4bd90e6375efa54f0bc254f9fe
MATVEN <mattiavena2993@gmail.com>
2019-11-03 11:53:40 +0100

    edit error

--- js/main.js
+++ js/main.js
@@ -1,23 +1,56 @@
 //Creare un oggetto che descriva uno studente con le seguenti proprietà: nome, cognome e età.
 var student = {
-  "nome":[],
-  "cognome":[],
-  "età":[]
+  'name' : 'Mattia',
+  'surname' : 'Vena',
+  'age' : '26'
 };
+
 //Stampare a schermo attraverso il for in tutte le proprietà.
-for (var key in studentt) {
-  console.log(key);
+for (var property in student) {
+  console.log(property, ': ',student[property]);
 }
-
 //Creare un array di oggetti di studenti.
-var name = [];
-var age = [];
-var surname = [];
-//Ciclare su tutti gli studenti e stampare per ognuno nome e cognome
-while () {
+// creo altri studenti
+var student2 = {
+  'name' : 'Marco',
+  'surname' : 'Trinca',
+  'age' : '28'
+};
 
+var student3 = {
+  'name' : 'Federico',
+  'surname' : 'Egidi',
+  'age' : '25'
+};
+
+var student4 = {
+  'name' : 'Francesco',
+  'surname' : 'Totti',
+  'age' : '22'
+};
+
+// li metto nell'array
+var arrayStudent = [student, student2, student3, student4];
+
+
+//Ciclare su tutti gli studenti e stampare per ognuno nome e cognome
+for (var i = 0; i < arrayStudent.length; i++) {
+  console.log(arrayStudent[i].name, arrayStudent[i].surname);
 }
+
 //Dare la possibilità all’utente attraverso 3 prompt di aggiungere un nuovo oggetto studente inserendo nell’ordine: nome, cognome e età.
-var nameUser = prompt('Inserisci il tuo nome.')
-var surnameUser = prompt('Insrisci il tuo cognome')
-var ageUse = parseInt('Inserisci la tua età.')
+// chiedo nome, cognome e età per inserire nuovo studente
+var newName = prompt('Inserisci il tuo nome');
+var newSurname = prompt('Inserisci il tuo cognome');
+var newAge = prompt('Inserisci la tua età');
+
+// creo un nuovo studente con dati inseriti dall'utente
+var student4 = {
+  'name' : newName,
+  'surname' : newSurname,
+  'age' : newAge
+};
+
+// aggiungo nuovo studente nell' arrayStudent
+arrayStudent.push(student4);
+console.log(arrayStudent);
